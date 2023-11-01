335efd3109e935672a1eb8297cc5b34a1929a9bc
Karinamcustodio <karinamoreiracustodio@gmail.com>
2023-11-01 17:27:27 -0300

    Exercicio de lista com cadastro de carro, adicionar, listar, editar e excluir

---
+++ Car.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPOOcar
+{
+    class Car
+    {
+        private string marca;
+        private string modelo;
+        private string cor;
+        private string placa;
+    
+        public Car()
+        {
+            //
+        }
+
+        public Car(string marca, string modelo, string cor, string placa)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+            Placa = placa;
+        }
+
+        public Car(string marca, string modelo, string cor)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cor = cor;
+        }
+
+        public Car(string modelo, string placa)
+        {
+            Modelo = modelo;
+            Placa = placa;
+        }
+
+        public string displayCar()
+        {
+            return $"Marca: {marca}, Modelo: {modelo}, Cor: {cor}, Placa: {placa}";
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("O nome da marca precisa ter no mínimo três letras");
+                }
+                else
+                {
+                    marca = value;
+                }
+            }
+        }
+        public string Modelo
+        {
+            get { return modelo; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    Console.WriteLine("O nome do modelo precisa ter no mínimo quatro letras");
+                }
+                else
+                {
+                    modelo = value;
+                }
+            }
+        }
+
+        public string Cor
+        {
+            get { return cor; }
+            set
+            {
+                if (value.Length < 4)
+                {
+                    Console.WriteLine("O nome do modelo precisa ter no mínimo quatro letras");
+                }
+                else
+                {
+                    cor = value;
+                }
+            }
+        }
+
+        public string Placa
+        {
+            get { return placa; }
+            set
+            {
+                if (value.Length < 7)
+                {
+                    Console.WriteLine("A placa precisa ter no mínimo sete letras");
+                }
+                else
+                {
+                    placa = value;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AulaPOOcar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AdicionarCar();
+            //VerificarCadastroCar();
+            //ListCar();
+
+            List<Car> cars = new List<Car>();
+
+            while (true)
+            {
+                Console.WriteLine("Infome uma opção");
+                Console.WriteLine("1 = Cadastre seu carro");
+                Console.WriteLine("2 = Consulte seu cadastro");
+                Console.WriteLine("3 = Edite seu cadastrro");
+                Console.WriteLine("4 = Exclui seu cadastro");
+                Console.WriteLine("5 = Consulte um cadastro pela placa");
+
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        AdicionarCar(cars);
+                        break;
+
+                    case 2:
+                        VerificarCadastroCar(cars);
+                        break;
+                        
+                    case 3:
+                        EditarCadastroCar();
+                        break;
+
+                    case 4:
+                        ExcluirCadastroCar();
+                        break;
+
+                    case 5:
+                        ConsultarPlacaCadastroCar();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void AdicionarCar(List<Car> origin)
+        {
+            bool repetir = true;
+            int contador = 0;
+
+                Console.WriteLine("Vamos cadastrar seu carro!");
+
+            while (repetir)
+            {
+                Console.WriteLine("Informe a marca do carro:");
+                string marca = Console.ReadLine();
+                
+                Console.WriteLine("Informe o modelo do carro:");
+                string modelo = Console.ReadLine();
+                
+                Console.WriteLine("Informe a cor do carro:");
+                string cor = Console.ReadLine();
+               
+                Console.WriteLine("Informe a placa do carro:");
+                string placa = Console.ReadLine();
+
+                origin.Add(new Car(marca, modelo, cor, placa));
+
+                Console.WriteLine("Deseja cadastrar mais um carro? s = sim  n = não");
+                string opcao = Console.ReadLine();
+                repetir = opcao.ToLower().Equals("s");
+                contador++;
+            }
+        }
+
+        static void VerificarCadastroCar(List<Car> origin)
+        {
+            Console.WriteLine("Segue informações do seu cadastro!");
+
+            foreach (Car car in origin)
+            {
+                Console.WriteLine(car.displayCar());
+                break;
+            }
+        }
+
+        static void EditarCadastroCar()
+        {
+
+        }
+
+        static void ExcluirCadastroCar()
+        {
+
+        }
+
+        static void ConsultarPlacaCadastroCar()
+        {
+
+        }
+
+        static void ListCar()
+        {
+            List<Car> cars = new List<Car>();
+
+            cars.Add(new Car() { Marca = "Fiat", Modelo = "Mobi", Cor = "Branco", Placa = "QJP8877" });
+            cars.Add(new Car() { Marca = "Volkswagen", Modelo = "Gol", Cor = "Preto", Placa = "DLM4562" });
+            cars.Add(new Car() { Marca = "Chevrolet", Modelo = "Onix", Cor = "Vermelho"});
+            cars.Add(new Car() { Modelo = "Tracker", Placa = "HJR7648" });
+        }
+    }
+}
